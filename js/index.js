d3832d61e55e1b5f0fe4f9a64d44c9d023202733
Chris Mayer <mayerxc@gmail.com>
2017-11-29 12:32:19 -0500

    cleaning up code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 $( document ).ready(function() {
-  $("button").click(function () {
+
+  //when enter key is pressed, search input
+  $('#search').keyup(function(e){
+    if (e.which === 13) {
+      $('#searchButton').click();  
+    }  
+  })
+  
+  //search function for input
+  $("#searchButton").click(function () {
     var search = $("input").val();
-    console.log("this is what is in the search box! " + search);  
+    console.log("this is what is in the search box: " + search);  
     var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&format=json&callback=?";
     console.log("API: "+api);
     $.ajax({
@@ -9,24 +18,31 @@ $( document ).ready(function() {
       type: "GET",
       dataType: 'json',
       success: function( data ) {
+        //empty previous search
+        console.log(data);
         $('#output').empty();
-        for (var i=0;data[1].length>i; i++){
+        for (var i = 0; data[1].length > i; i++) {
           var well = document.createElement('div');
           var dt = document.createElement("dt");
           var dd = document.createElement('dd');
           $(well).addClass("well");
-          dt.innerHTML = "<a href = " + data[3][i] + " target = 'blank'>" + data[1][i] + "</a>";
+          dt.innerHTML = "<a href = " + data[3][i] + " target='blank'>" + data[1][i] + "</a>";
           dd.innerHTML = data[2][i];
-          
           $('#output').append(well);
           $(well).append(dt).append(dd);
           
         }//end for 
       }, 
       error: function ( error ) {
-        alert ('Error with API');
+        alert('Error with API');
       }
     });//end of ajax
   });//end search click
-  
+
+  //set up function when random button is clicked
+  $('#random').click(function(){
+    window.open('http://en.wikipedia.org/wiki/Special:Random');
+  })
+
+
 });//end doc ready
